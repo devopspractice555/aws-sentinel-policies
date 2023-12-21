bcd7016b3b6a2feeed89349f809b587a5389093b
devopspractice555 <devopspractice555@gmail.com>
2023-12-21 15:18:11 +0530

    new files

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "aws-practice1"
+
+    workspaces {
+      name = "aws-sentinel-policies"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-east-1"
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-0a0f1259dd1c90938"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
